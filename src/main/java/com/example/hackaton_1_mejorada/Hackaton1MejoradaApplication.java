63a53f154f415321d74d3b3baf5a59a6174837a0
FaridAquino <farid.aquino@utec.edu.pe>
2025-05-06 17:18:52 -0500

    añadiendo archivos

---
+++ src/main/java/com/example/hackaton_1_mejorada/Hackaton1MejoradaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackaton_1_mejorada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackaton1MejoradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackaton1MejoradaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hackaton_1_mejorada/Hackaton1MejoradaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackaton_1_mejorada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackaton1MejoradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
